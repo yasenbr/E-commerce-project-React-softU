7d740b078d577cb7b7820d218c9050813cef624a
yasenbr <74763207+yasenbr@users.noreply.github.com>
2021-03-18 23:13:24 +0200

    create config.js
    add fire base

---
+++ components/config/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage ";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBwD9FqGShYaVr9McHeeNleG2H7xxfjkBs",
+  authDomain: "e-commerce-project-react-softu.firebaseapp.com",
+  projectId: "e-commerce-project-react-softu",
+  storageBucket: "e-commerce-project-react-softu.appspot.com",
+  messagingSenderId: "197163523148",
+  appId: "1:197163523148:web:e7c43e9f3e0fb073ad68e9",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+export { auth, db, storage };
