919d169ff1538c211e5861ac6126f11fbada157c
yasenbr <74763207+yasenbr@users.noreply.github.com>
2021-03-29 20:27:15 +0300

    remove non used file

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,7 +27,7 @@ export const Login = (props) => {
       <Navibar />
       <div className="">
         <div className="container">
-          <h4 className="text-center pt-5 mt-5 mb-5">Login</h4>
+          <h1 className="text-center pt-5 mt-5 mb-5">Login</h1>
         </div>
       </div>
       <br />
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -44,7 +44,7 @@ export const Signup = (props) => {
       <Navibar />
       <div className="">
         <div className="container">
-          <h4 className="text-center pt-5 mt-5 mb-5">Sign up</h4>
+          <h1 className="text-center pt-5 mt-5 mb-5">Sign up</h1>
         </div>
       </div>
       <br />
